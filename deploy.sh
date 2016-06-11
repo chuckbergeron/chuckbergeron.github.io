646b1bb112a1d9c95f8bea6acba19f6e82264933
Charles Bergeron <chuck.bergeron@gmail.com>
2016-06-10 22:24:39 -0700

    switch deploy script to use source as the jekyll stuff and master as the compiled final copy

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,19 @@ read -rsp $'... Press any key to continue or control-c to cancel...\n';
 
 export JEKYLL_ENV=production
 
-git checkout 'master'
+git checkout 'source'
 bundle exec jekyll build
 git add .
 git commit -a -m "deployed new version"
-git push origin 'master'
+git push origin 'source'
 
 rm -rf /tmp/_site
 cp -r _site /tmp/_site
-git checkout 'gh-pages'
+git checkout 'master'
 cp -r /tmp/_site/* .
 git add .
 git commit -a -m "deployed new version"
-git push -f origin 'gh-pages'
-git checkout 'master'
+git push -f origin 'master'
+git checkout 'source'
 
 export JEKYLL_ENV=development
